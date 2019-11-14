afebffaca31e7a789caa99b719f69189a0de6f2b
chrisshepard <43590660+chrisshepard@users.noreply.github.com>
2019-11-14 14:48:54 -0500

    final

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let aboutLink = document.getElementById('about-link');
 aboutLink.addEventListener('click', ()=>{
     aboutLink.className = 'animated zoomOut slow';
 })
+
+let page = document.querySelector('.starter');
+let textSizeToggler = document.getElementById('toggler');
+
+textSizeToggler.addEventListener('click', ()=>{
+    textSizeToggler.parentNode.classList.toggle('larger');
+})
